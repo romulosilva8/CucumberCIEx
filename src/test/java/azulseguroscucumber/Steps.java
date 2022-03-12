4ce7d43c7e4749978275633f9afe7ae2ffd65040
Matheus Vieira <matheus-vieira12@hotmail.com>
2022-03-12 19:13:19 -0300

    commit

--- src/test/java/azulseguroscucumber/Steps.java
+++ src/test/java/azulseguroscucumber/Steps.java
@@ -1,10 +1,21 @@
 package azulseguroscucumber;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
 
 public class Steps {
+	
+	
 	@Dado("A comunicacao do aviso de emissao")
 	public void a_comunicacao_do_aviso_de_emissao() {
 	    System.out.println("passou");
@@ -20,4 +31,31 @@ public class Steps {
 		System.out.println("passou");
 	}
 
+	public static void zipFile() {
+		String filePath = System.getProperty("user.dir") + "/target/cucumber.json";
+		
+		try {
+			File file = new File(filePath);
+			String zipFileName = file.getName().concat(".zip");
+			
+			FileOutputStream fos = new FileOutputStream(zipFileName);
+			ZipOutputStream zos = new ZipOutputStream(fos);
+			
+			zos.putNextEntry(new ZipEntry(file.getName()));
+			
+			byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+			zos.write(bytes, 0, bytes.length);
+			zos.closeEntry();
+			zos.close();
+			
+		} catch (FileNotFoundException ex) {
+			System.err.format("The file %s does not exist", filePath);
+		} catch (IOException ex) {
+			System.err.println("I/O error: " + ex);
+		}
+	}
+	
+	public Steps() {
+		zipFile();
+	}
 }
--- src/test/java/azulseguroscucumber/TestRunner.java
+++ src/test/java/azulseguroscucumber/TestRunner.java
@@ -1,5 +1,14 @@
 package azulseguroscucumber;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
@@ -7,8 +16,9 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features", glue = {"azulseguroscucumber"}, monochrome = true,
-plugin = {"pretty", "json:target/results.zip"}
+plugin = {"pretty", "json:target/cucumber.json"}
 )
 public class TestRunner {
+ 
 
 }
