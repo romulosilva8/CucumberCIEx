3b56fa6e03b775baa0bc2e02aa0c3841265e209f
Matheus Vieira <matheus-vieira12@hotmail.com>
2022-03-12 17:27:49 -0300

    commit

--- src/test/java/azulseguroscucumber/TestRunner.java
+++ src/test/java/azulseguroscucumber/TestRunner.java
@@ -6,8 +6,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src\\test\\resources\\Features", glue = {"azulseguroscucumber"}, monochrome = true,
-plugin = {"pretty", "junit:target\\JUnitReports\\Report.xml"}
+@CucumberOptions(features="src/test/resources/Features", glue = {"azulseguroscucumber"}, monochrome = true,
+plugin = {"pretty", "junit:target/JUnitReports/Report.xml"}
 )
 public class TestRunner {
 
